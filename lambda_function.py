71d07fd9c48d00d750c6074628dd581dda69611c
heycristhian <heycristhian@gmail.com>
2023-10-01 22:14:31 -0300

    Iniciando primeira versão da aplicação

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,20 @@
-import json
+import boto3
 
 
 def lambda_handler(event, context):
-    # TODO implement
+    client = boto3.client('ce', region_name='us-east-1')
+    response = client.get_cost_and_usage(
+        TimePeriod={
+            'Start': '2023-01-01',
+            'End': '2023-01-31'
+        },
+        Granularity='MONTHLY',
+        Metrics=['UnblendedCost']
+    )
+
+    cost = response['ResultsByTime'][0]['Total']['UnblendedCost']['Amount']
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! This is my first deploy!')
+        'body': f'O custo do mês atual é de ${cost}'
     }
