6d20387c165594a4b864444f159c1f1b354d51c6
heycristhian <heycristhian@gmail.com>
2023-10-04 22:06:04 -0300

    Inserido mais uma visualizacao de valor

--- lambda_function.py
+++ lambda_function.py
@@ -3,11 +3,28 @@ from datetime import datetime, timedelta
 import boto3
 import dateutil.tz
 
+brazil_timezone = dateutil.tz.gettz('America/Sao_Paulo')
+
 
 def lambda_handler(event, context):
     client = boto3.client('ce', region_name='us-east-1')
 
-    current_date, last_month_date = get_dates()
+    cost = get_cost(client)
+    forecasted_cost = get_forecasted_cost(client)
+
+    msg = (f'AWS Billing: Previsão total do mês atual é de ${forecasted_cost}. '
+           f'Valor atual é de ${cost}.')
+
+    send_email(msg)
+
+    return {
+        'statusCode': 200,
+        'body': msg
+    }
+
+
+def get_cost(client):
+    current_date, last_month_date = get_dates_cost()
 
     response = client.get_cost_and_usage(
         TimePeriod={
@@ -18,23 +35,28 @@ def lambda_handler(event, context):
         Metrics=['UnblendedCost']
     )
 
-    cost = round(float(response['ResultsByTime'][0]['Total']['UnblendedCost']['Amount']), 2)
-
-    msg = f'AWS Billing: Previsão total do mês atual é de ${cost}'
-
-    send_email_response = send_email(msg)
-
-    after_print(last_month_date, current_date, send_email_response)
-
-    return {
-        'statusCode': 200,
-        'body': msg
-    }
+    return round(float(response['ResultsByTime'][0]['Total']['UnblendedCost']['Amount']), 2)
 
 
-def get_dates():
+def get_forecasted_cost(client):
+    today = datetime.now(brazil_timezone)
+    next_day_of_current_month = datetime(today.year, today.month, today.day + 1)
+    first_day_next_month = datetime(today.year, today.month + 1, 1)
+
+    response = client.get_cost_forecast(
+        TimePeriod={
+            'Start': next_day_of_current_month.strftime('%Y-%m-%d'),
+            'End': first_day_next_month.strftime('%Y-%m-%d')
+        },
+        Metric='UNBLENDED_COST',
+        Granularity='MONTHLY'
+    )
+
+    return round(float(response['Total']['Amount']), 2)
+
+
+def get_dates_cost():
     date_format = '%Y-%m-%d'
-    brazil_timezone = dateutil.tz.gettz('America/Sao_Paulo')
 
     current_date = datetime.now(brazil_timezone)
     last_month_date = current_date - timedelta(days=30)
@@ -59,9 +81,3 @@ def send_email(message):
             }
         }
     )
-
-
-def after_print(last_month_date, current_date, send_email_response):
-    print('last_month_date: ' + str(last_month_date))
-    print('current_date: ' + str(current_date))
-    print('send_email_response: ' + str(send_email_response))
